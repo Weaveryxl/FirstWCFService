e0d96a4c7182e60bab853128fcd11af572fc199d
Weaveryxl <weaveryxl549@gmail.com>
2017-05-12 13:38:30 -0400

    FirstWCFService

---
+++ FirstWCFService/HelloWorldService.svc.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace FirstWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(Namespace = "http://antra.net")]
+    public class HellowordlService : IHelloworldService
+    {
+        public string GetEmployeeName(int ID)
+        {
+            try
+            {
+                return "My name is John Doe";
+            }
+            catch (Exception)
+            {
+                var fault = new HelloFault
+                {
+                    ErrorCode = "99",
+                    ErrorDescription = "Please check values"
+                };
+                throw new FaultException<HelloFault>(fault);
+            }
+        }
+
+        public Employee GetEmployee(int ID)
+        {
+            try
+            {
+                var allEmps = GetAllEmployees();
+                return allEmps.First(x => x.ID == ID);
+            }
+            catch (Exception ex)
+            {
+                var fault = new HelloFault
+                {
+                    ErrorCode = "100",
+                    ErrorDescription = ex.Message
+                };
+                throw new FaultException<HelloFault>(fault);
+            }
+        }
+
+        public int Divide(int a, int b)
+        {
+            try
+            {
+                return a/b;
+            }
+            catch (DivideByZeroException ex)
+            {
+                var fault = new HelloFault
+                {
+                    ErrorCode = "101",
+                    ErrorDescription = "Divide by Zero"
+                };
+                throw new FaultException<HelloFault>(fault);
+            }
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a*b;
+        }
+
+        private List<Employee> GetAllEmployees()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {ID = 1, Name = "John", State = "VA"},
+                new Employee {ID = 2, Name = "Tom", State = "CT"},
+                new Employee {ID = 3, Name = "Kate", State = "OH"}
+            };
+            return employees;
+        }
+    }
+}
---
+++ FirstWCFService/IHelloworldService.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace FirstWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Namespace = "http://antra.net")]
+    public interface IHelloworldService
+    {
+        [OperationContract]
+        [FaultContract(typeof(HelloFault))]
+        string GetEmployeeName(int ID);
+
+        [OperationContract]
+        [FaultContract(typeof(HelloFault))]
+        Employee GetEmployee(int ID);
+
+        [OperationContract]
+        [FaultContract(typeof(HelloFault))]
+        int Divide(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(HelloFault))]
+        int Multiply(int a, int b);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+         
+        [IgnoreDataMember]
+        public string SSN { get; set; }
+        
+    }
+
+    [DataContract(Namespace = "http://antra.net")]
+    public class HelloFault
+    {
+        [DataMember]
+        public string ErrorCode { get; set; }
+
+        [DataMember]
+        public string ErrorDescription { get; set; }
+    }
+}
